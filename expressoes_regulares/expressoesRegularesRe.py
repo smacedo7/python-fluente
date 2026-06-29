dc534a82cd68235e94915ecee282653f8c0397b4
Samuel Macedo <185958358+smacedo7@users.noreply.github.com>
2026-06-28 23:47:13 -0300

    regular expressions

---
+++ expressoes_regulares/expressoesRegularesRe.py
@@ -0,0 +1,17 @@
+import re
+
+phone_num_pattern_obj = re.compile(r'\d{3}-\d{3}-\d{4}')  # passa string regula
+match_obj = phone_num_pattern_obj.search('My number is 415-555-4242')
+match_obj.group()
+
+# 4 passos:
+# 1- importar modulo re
+# 2- passar a string regex para re.compile() para obter objeto pattern
+# 3- passar a string de texto para metodo search() do objeto pattern para
+# obter um objeto match
+# 4- chamar o metodo group do objeto match para obter a string que corresponde
+# ao padrao desejado
+
+# re.compile
+# var.search
+# var.group
