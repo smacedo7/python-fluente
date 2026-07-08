52dc30cd3bbad5324991bf4ae86af6a60cd373ef
Samuel Macêdo Correia <samucamaiscedo@gmail.com>
2026-07-08 01:50:41 -0300

    finalizando decorator

--- decoradores_exercicio.py
+++ decoradores_exercicio.py
@@ -1,12 +1,14 @@
 def log(func):
-    def interna(*args, **kwargs):
+    def interna(*args, **kwargs):  # empacota aqui
         print(f"executando {func.__name__}")
-        resultado = func(*args, **kwargs)
+        resultado = func(*args, **kwargs)  # desempacota aqui
         print("fim da execução da interna")
         return resultado
     print("fora da interna e agora retornando ela")
     return interna
 
+# mesma coisa que:
+# soma = log(soma) # aponta pra interna aqui
 @log
 def soma(a, b):
     print("entrei na função da soma")
@@ -14,4 +16,11 @@ def soma(a, b):
 
 print(soma(3, 4))
 
+
 # primeiro a ser executado é a decoradora
+
+# fora da interna e agora retornando ela
+# executando soma
+# entrei na função da soma
+# fim da execução da interna
+# 7
