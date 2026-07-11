0e213046946f342cc0e0c826ce987f534b2a0c7d
Samuel Macêdo Correia <samucamaiscedo@gmail.com>
2026-07-11 19:12:30 -0300

    ex dunder methods

--- ex_vetor.py
+++ ex_vetor.py
@@ -2,20 +2,55 @@ class Vetor:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-
-    def __add__(self, other):
-        return 
     
+    def __add__(self, other):
+        new_x = self.x + other.x
+        new_y = self.y + other.y
+
+        return Vetor(new_x, new_y)
+
     def __eq__(self, value):
-        pass
-
+        if not isinstance(value, Vetor):
+            return NotImplemented
+        # if self.x == value.x and self.y == value.y:
+        #     return True
+        # else:
+        #     return False
+        return self.x == value.x and self.y == value.y
+    
     def __len__(self):
-        return len(self)
-
+        # return len([self.x, self.y])  # nao necesasriamente precisava retornar um len
+        # mas sempre um numero inteiro
+        return 2
+    
+    def __repr__(self):
+        return f"Vetor: (x={self.x}, y={self.y})"
+    
 v1 = Vetor(2, 3)
 v2 = Vetor(5, 7)
-print(v1)
-v3 = v1 + v2
+v3 = Vetor(2, 3)
+v4 = Vetor(3, 2)
 
-print(v3.x)
-print(v3.y)
+print("=== __repr__ ===")
+print(v1)
+print(v2)
+
+print("\n=== __add__ ===")
+resultado = v1 + v2
+print(resultado)
+
+print("\n=== __eq__ ===")
+print(v1 == v2)   # False
+print(v1 == v3)   # True
+print(v1 == v4)   # False
+
+print("\n=== __len__ ===")
+print(len(v1))
+print(len(v2))
+
+print("\n=== Objetos originais ===")
+print(v1)
+print(v2)
+
+# print("\n=== Desafio ===")
+# print(v1 + 10)
